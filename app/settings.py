342d62ccf86c767ca5f3500be0c4f8d3d08a30bc
vidya sagar <60987364+vidyasagar1432@users.noreply.github.com>
2022-10-10 10:17:26 +0530

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -2,11 +2,8 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     HOST:str = 'http://127.0.0.1:8000'
-    DETA_PROJECT_KEY:str
+    DETA_PROJECT_KEY:str = Field(..., env='DETA_PROJECT_KEY')
     DETA_BASE_NAME:str = 'links'
 
-    class Config:
-        env_file = '.env'
-        env_file_encoding = 'utf-8'
 
-setting = Settings(_env_file='.env')
+setting = Settings()
